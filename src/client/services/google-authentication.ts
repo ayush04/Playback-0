bc781f4c5122747ea85b5c88cf906d8ac3736268
ayush <jain.ayush12@gmail.com>
2020-05-19 22:57:26 +0100

    Added apis to fetch secret

--- src/client/services/google-authentication.ts
+++ src/client/services/google-authentication.ts
@@ -8,23 +8,27 @@ export class GoogleAuthentication {
     static authenticate(): Promise<any> {
         return new Promise((resolve, reject) => {
             gapi.load('auth2', () => {
-                // @ts-ignore
-                gapi.auth2.init({
-                    client_id: '<client_id>'
-                });
-                
-                // @ts-ignore
-                return gapi.auth2.getAuthInstance().signIn({
-                    scope: 'https://www.googleapis.com/auth/youtube.force-ssl'
-                }).then(() => {
-                    GoogleAuthentication._isAuthenticated = true;
-                    window.sessionStorage.setItem('google-authenticated', 'true');
-                    console.log('Authenticated');
-                    return resolve(GoogleAuthentication.loadClient());
-                }).catch((err: Error) => {
-                    console.log(err);
-                    return reject(true);
-                });
+                fetch('http://playback.io:3000/secret/id')
+                    .then(response => response.json())
+                    .then(responseJson => {
+                        // @ts-ignore
+                        gapi.auth2.init({
+                            client_id: responseJson.CLIENT_ID
+                        });
+                        
+                        // @ts-ignore
+                        return gapi.auth2.getAuthInstance().signIn({
+                            scope: 'https://www.googleapis.com/auth/youtube.force-ssl'
+                        }).then(() => {
+                            GoogleAuthentication._isAuthenticated = true;
+                            window.sessionStorage.setItem('google-authenticated', 'true');
+                            console.log('Authenticated');
+                            return resolve(GoogleAuthentication.loadClient());
+                        }).catch((err: Error) => {
+                            console.log(err);
+                            return reject(true);
+                        });
+                    });
             });
         });
     }
@@ -32,8 +36,12 @@ export class GoogleAuthentication {
     static loadClient() {
         return new Promise((resolve, reject) => {
             gapi.load('client', () => {
-                gapi.client.setApiKey('<api_key>');
-                return resolve(gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest", "v3"));
+                fetch('http://playback.io:3000/secret/key')
+                    .then(response => response.json())
+                    .then(responseJson => {
+                        gapi.client.setApiKey(responseJson.API_KEY);
+                        return resolve(gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest", "v3"));
+                    });
             });
         });
     }
